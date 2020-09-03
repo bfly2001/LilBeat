827d8ed2e3471f8675e935e2639654b913f56019
bfly2001 <brendan.sflynn@gmail.com>
2020-09-03 16:55:01 -0700

    Update script.js
    created oscillator node

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 let audioContext = new (window.AudioContext || window.webkitAudioContext)();
 
+
 const bass = document.querySelector('bassdrum');
 const snare = document.querySelector('snaredrum');
 const hat = document.querySelector('hatdrum');
+const oscillator = audioContext.createOscillator();
 
 bass.addEventListener('click', function() {
     
