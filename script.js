708d51e63523a539c79ce4a68a0c64a818bea936
bfly2001 <brendan.sflynn@gmail.com>
2020-09-02 21:55:58 -0700

    Update script.js
    created constant variables

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 let audioContext = new (window.AudioContext || window.webkitAudioContext)();
+
+const bass = document.querySelector('bassdrum');
+const snare = document.querySelector('snaredrum');
+const hat = document.querySelector('hatdrum');
+
+bass.addEventListener('click', function() {
+    
+})
+
+/*
 const bass = new Audio();
 const snare = new Audio();
 const hat = new Audio();
