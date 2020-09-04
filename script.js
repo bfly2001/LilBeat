3d374ee79aba12454c681cf7b2ff9cccf8a34532
bfly2001 <brendan.sflynn@gmail.com>
2020-09-04 15:42:52 -0700

    Update script.js
    -Selected constants using getElementById instead of using querySelector.
    
    -Added EventListeners to bass, snare, and hat variables.

--- script.js
+++ script.js
@@ -1,11 +1,43 @@
 let audioContext = new (window.AudioContext || window.webkitAudioContext)();
 
 
-const bass = document.querySelector('bassdrum');
-const snare = document.querySelector('snaredrum');
-const hat = document.querySelector('hatdrum');
+const bass = document.getElementById('bassdrum');
+const snare = document.getElementById('snaredrum');
+const hat = document.getElementById('hatdrum');
 const synth = document.getElementById('synth');
 
+bass.addEventListener('click', function() {
+    let audioContext = new AudioContext();
+    let oscillator = audioContext.createOscillator();
+    oscillator.type = 'sine';
+    oscillator.frequency.value = 50;
+    oscillator.connect(audioContext.destination);
+    oscillator.start();
+    oscillator.stop(0.1);
+    audioContext.resume();
+});
+
+snare.addEventListener('click', function() {
+    let audioContext = new AudioContext();
+    let oscillator = audioContext.createOscillator();
+    oscillator.type = 'sawtooth';
+    oscillator.frequency.value = 300;
+    oscillator.connect(audioContext.destination);
+    oscillator.start();
+    oscillator.stop(0.1);
+    audioContext.resume();
+});
+
+hat.addEventListener('click', function() {
+    let audioContext = new AudioContext();
+    let oscillator = audioContext.createOscillator();
+    oscillator.type = 'square';
+    oscillator.frequency.value = 500;
+    oscillator.connect(audioContext.destination);
+    oscillator.start();
+    oscillator.stop(0.1);
+    audioContext.resume();
+});
 
 synth.addEventListener('click', function() {
     let audioContext = new AudioContext();
